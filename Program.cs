d0ca662653f9cd6deb45a3260497588e343bee77
Yuvaranjani123 <47520340+Yuvaranjani123@users.noreply.github.com>
2023-03-08 18:16:47 +0530

    Updated Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Enter your name?");
+﻿Console.WriteLine("Enter your full name?");
 Console.WriteLine("Enter your age?");
 var name = Console.ReadLine();
 var currentDate = DateTime.Now;
