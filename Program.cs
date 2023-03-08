8561f93d9e068a52f59b6fe0205d300337eb1610
Yuvaranjani123 <47520340+Yuvaranjani123@users.noreply.github.com>
2023-03-08 18:17:10 +0530

    changes in Program.cs file

--- Program.cs
+++ Program.cs
@@ -2,6 +2,6 @@ Console.WriteLine("Enter  name?");
 Console.WriteLine("Enter address?");
 var name = Console.ReadLine();
 var currentDate = DateTime.Now;
-Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+Console.WriteLine($"{Environment.NewLine}Hello, {name}, how are you on {currentDate:d} at {currentDate:t}!");
 Console.Write($"{Environment.NewLine}Press any key to exit...");
 Console.ReadKey(true);
