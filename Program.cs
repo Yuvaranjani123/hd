e535de5b99dc2019a64d19ae76560a1a006a9d91
Yuvaranjani123 <47520340+Yuvaranjani123@users.noreply.github.com>
2023-03-08 18:17:03 +0530

    Updated Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 Console.WriteLine("Enter  name?");
-Console.WriteLine("Enter age?");
+Console.WriteLine("Enter age in text?");
 var name = Console.ReadLine();
 var currentDate = DateTime.Now;
 Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
